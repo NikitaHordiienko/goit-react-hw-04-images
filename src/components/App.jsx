6ec5d42737dd78973047d3fd880c5c5b920afd87
NikitaHordiienko <nikita.gordienko34@gmail.com>
2022-10-15 13:10:31 +0300

    Fixed error warning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,7 +47,7 @@ export default function App() {
         setStatus(Status.REJECTED);
       }
 
-    } catch (error) {
+    } catch (setError) {
       console.log(error) 
       setError(error)
     }
